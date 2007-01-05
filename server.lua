21794a67dd2753de86d2e9e82902ec8296064243
polarityattraction <polarityattraction@9705f834-4926-0410-b3e1-1f897740c9a0>
2007-01-05 12:52:13 +0000

    Commit for IRC check.

--- server.lua
+++ server.lua
@@ -17,6 +17,44 @@ recvt = {}
 --Add our initial connection to the table
 table.insert(recvt,listening)
 
+--Create our commands table
+commands = {}
+
+--Setup command handlers
+function ParseCommand(command)
+	if commands[command] then
+		if commands[command].callback then
+			commands[command].callback()
+		end
+		if commands[command].response then
+			return commands[command].response
+		end
+	else
+		return nil
+	end
+end
+
+function AddCommand(command, response, callback)
+	if command then
+		commands[command] = {}
+		if response or callback then
+			if callback then
+					commands[command].callback = callback
+			end
+			if response then
+				commands[command].response = response
+			end
+		else
+			print("LuaMMO: Error adding command. No callback or response specified when adding function "..command..".")
+		end
+	else
+		print("LuaMMO: Not enough args to AddCommand request")
+	end
+end
+
+--Create default commands
+AddCommand("HELLO","Hey there!")
+
 --Loop forever
 while true do
 
@@ -30,15 +68,9 @@ while true do
 	else
 		for _, server in ipairs(reading) do
 			local message = server:receive("*l")
+			print(message) --Debuugging only.
 			server:send(ParseCommand(message))
 		end
 	end
-	
-end
-
---Parse command from a client. (Got this ready for later)
-function ParseCommand(command)
-
-	if command == "HELLO" then return "HELLO" end
 	
 end
